aa0b9fa37dcbd2498af1e89818f603d577c7a7d2
dcs3spp <simonppears@googlemail.com>
2020-01-17 17:05:11 +0000

    chore(build): fix build

--- src/donot-__mocks__/typesafe-actions.ts
+++
@@ -1,8 +0,0 @@
-// __mocks__/typesafe-actions.ts
-'use strict';
-
-const typesafe = jest.requireActual('typesafe-actions');
-
-(typesafe as any).createAsyncAction = jest.fn();
-
-module.exports = typesafe;
